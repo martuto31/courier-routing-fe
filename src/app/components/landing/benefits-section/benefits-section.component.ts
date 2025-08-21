657dad26879d7a84cf096fea57330b393e6cad07
Martin Petrov <petrovmarti21@gmail.com>
2025-08-21 03:08:27 +0300

    Change texts

--- src/app/components/landing/benefits-section/benefits-section.component.ts
+++ src/app/components/landing/benefits-section/benefits-section.component.ts
@@ -20,7 +20,7 @@ export class BenefitsSectionComponent {
 
   public benefits: BenefitsItem[] = [
     { text: 'По-бързи доставки', iconSrc: 'assets/img/lightning-icon.png', iconAlt: 'Lightning icon'},
-    { text: 'По-ниски разходи за гориво', iconSrc: 'assets/img/fuel-icon.png', iconAlt: 'Fuel icon'},
+    { text: 'По-нисък разход на гориво', iconSrc: 'assets/img/fuel-icon.png', iconAlt: 'Fuel icon'},
     { text: 'Повече доставени пратки', iconSrc: 'assets/img/update-icon.png', iconAlt: 'Update icon'},
     { text: 'Лесен за използване интерфейс', iconSrc: 'assets/img/interface-icon.png', iconAlt: 'Interface icon'},
   ];
